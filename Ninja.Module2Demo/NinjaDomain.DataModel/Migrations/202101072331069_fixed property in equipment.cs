f9f069176ab518e75b9c5cd66581fdbc21a29401
Carlos <clockwerkz@gmail.com>
2021-01-07 15:52:12 -0800

    Adds entity framework to Console app

---
+++ Ninja.Module2Demo/NinjaDomain.DataModel/Migrations/202101072331069_fixed property in equipment.cs
@@ -0,0 +1,16 @@
+﻿namespace NinjaDomain.DataModel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fixedpropertyinequipment : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
