0a4b1df6e4efbc336ac5945728ed3aa63adb0867
Carlos <clockwerkz@gmail.com>
2021-01-08 16:04:18 -0800

    Switching console app to .NET Framework

--- Ninja.Module2Demo/ConsoleApplication/Program.cs
+++ Ninja.Module2Demo/ConsoleApplication/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using NinjaDomain.Classes;
+using NinjaDomain.DataModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleApplication
 {
@@ -6,7 +12,23 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        }
+
+        private static void InsertNinja()
+        {
+            var ninja = new Ninja()
+            {
+                Name = "CarlosSan",
+                ServedInOniwaban = false,
+                DateOfBirth = new DateTime(1974, 11, 2),
+                ClanId = 1
+            };
+            using (var context = new NinjaContext())
+            {
+                context.Database.Log = Console.WriteLine; //Logging feature in Entity Framework
+                context.Ninjas.Add(ninja);
+                context.SaveChanges();
+            }
         }
     }
 }
